8082df1632e4d0659ed8753c70732f0c84d2685f
Max Wen <max.wen@qq.com>
2016-10-11 10:43:57 +0800

    welcome page

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -2,52 +2,77 @@
 
 /* @var $this yii\web\View */
 
-$this->title = Yii::$app->name;
+$this->title = '';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <h1>Yii2-adminLte-backend-with-Rbac</h1>
+        <p class="lead">欢迎使用</p>
+        <p><a class="btn btn-lg btn-default" href="https://github.com/max-wen" target="_blank"> <span class="fa fa-github"></span> Fork me on Github</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
+			<div class="col-sm-12"><h3>This yii2 applitcation integrates the following components(widgets): </h3></div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>yii2-admin</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>GUI manager for <strong class="text-danger">RABC</strong> (Role Base Access Control) Yii2. Easy to manage authorization of user.</p>
+				<p>Author: mdmsoft</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://github.com/mdmsoft/yii2-admin">Github page &raquo;</a></p>
             </div>
+
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>AdminLTE</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p><strong>AdminLTE</strong> -- is a fully responsive admin template. Based on Bootstrap 3 framework. Highly customizable and easy to use. Fits many screen resolutions from small mobile devices to large desktops. Check out the live preview now and see for yourself.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://github.com/almasaeed2010/AdminLTE">Github page &raquo;</a></p>
             </div>
+
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>yii2-adminlte-gii</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>yii2 code generator for AdminLTE theme. Wraps box widget for CRUD views and some theme adpter.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://github.com/max-wen/yii2-adminlte-gii">Github page &raquo;</a></p>
             </div>
         </div>
+
+		<div class="row">
+			<div class="col-lg-4">
+				<h2>yii2-ckeditor-widget</h2>
+
+				<p>CKEditor widget for yii2, with <strong class="text-danger">ckfinder</strong> integrated <br>
+					This repo was forked from 2amigos/yii2-ckeditor-widget.<br>
+					I made some customization and most impotant: with ckfinder integrated.</p>
+
+				<p><a class="btn btn-default" href="https://github.com/max-wen/yii2-ckeditor-widget">Github page &raquo;</a></p>
+			</div>
+
+			<div class="col-lg-4">
+				<h2>yii2-file-upload-widget</h2>
+
+				<p>Renders a <a href="http://blueimp.github.io/jQuery-File-Upload/">BlueImp jQuery File Upload plugin</a>.
+					That plugin integrates multiple file selection, drag&drop support, progress bars, validation and preview of images.</p>
+
+				<p><a class="btn btn-default" href="https://github.com/2amigos/yii2-file-upload-widget">Github page &raquo;</a></p>
+			</div>
+
+			<div class="col-lg-4">
+				<h2>yii2-gallery-widget</h2>
+
+				<p>Renders a BlueImp Gallery and/or Carousel. BlueImp Gallery is a touch-enabled, responsive and customizable image & video gallery, carousel and lightbox, optimized for both mobile and desktop web browsers. It features swipe, mouse and keyboard navigation, transition effects, slideshow functionality, fullscreen support and on-demand content loading and can be extended to display additional.
+
+					For more information, please visit <a href="http://blueimp.github.io/Gallery/">http://blueimp.github.io/Gallery/</a></p>
+
+				<p><a class="btn btn-default" href="https://github.com/2amigos/yii2-gallery-widget">Github page &raquo;</a></p>
+			</div>
+
+		</div>
 
     </div>
 </div>
